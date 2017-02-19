86b002ee7f1b038235b4de1df632afdb44387cda
Kresnadc <kresna_dcahyo@yahoo.co.id>
2017-02-19 21:39:56 +0700

    New Commit 19 2 2017

--- app/src/main/java/com/example/i14048/pkl/LoginActivity.java
+++ app/src/main/java/com/example/i14048/pkl/LoginActivity.java
@@ -1,5 +1,6 @@
 package com.example.i14048.pkl;
 
+import android.content.ContentValues;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -10,14 +11,17 @@ import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
 
-    EditText edUserName;
+    private EditText userNameEditText;
+    private EditText passwordEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        edUserName = (EditText) findViewById(R.id.editTextUserName);
+        userNameEditText = (EditText) findViewById(R.id.editTextUserName);
+        passwordEditText = (EditText) findViewById(R.id.editTextPassword);
+
         Button btnLogin = (Button) findViewById(R.id.buttonLogin);
         Button btnReg = (Button) findViewById(R.id.buttonRegistrasi);
 
@@ -25,24 +29,48 @@ public class LoginActivity extends AppCompatActivity {
 
             @Override
             public void onClick(View arg0){
-                Toast.makeText(getApplicationContext(), "Login berhasil ...", Toast.LENGTH_SHORT).show();
-                Intent i = new Intent(LoginActivity.this, KatalogActivity.class);
-                Bundle b = new Bundle();
-                b.putString("UserName", edUserName.getText().toString());
-                i.putExtras(b);
-                startActivity(i);
+                if(login()) {
+                    Toast.makeText(getApplicationContext(), "Login Success", Toast.LENGTH_SHORT).show();
+                    Intent i = new Intent(LoginActivity.this, KatalogActivity.class);
+                    Bundle b = new Bundle();
+                    b.putString("UserName", userNameEditText.getText().toString());
+                    i.putExtras(b);
+                    startActivity(i);
+                }else{
+                    Toast.makeText(getApplicationContext(), "Wrong Username or Password", Toast.LENGTH_SHORT).show();
+                }
             }
         });
 
         btnReg.setOnClickListener(new View.OnClickListener() {
-
             @Override
             public void onClick(View arg0){
-                Toast.makeText(getApplicationContext(), "Silahkan isi form Registrasi", Toast.LENGTH_SHORT).show();
-                Intent i = new Intent(LoginActivity.this, KatalogActivity.class);
-                Bundle b = new Bundle();
+                Toast.makeText(getApplicationContext(), "Please Fill Registration Form", Toast.LENGTH_SHORT).show();
+                Intent i = new Intent(LoginActivity.this, RegisterActivity.class);
                 startActivity(i);
             }
         });
     }
+
+    private boolean login(){
+        boolean isLoginSuccess = false;
+        ContentValues AccountInfo = new AccountDBHandler(this).getAccount(getUserNameText(), getPasswordText());
+        if(AccountInfo.get("email") == null){
+            Toast.makeText(getApplicationContext(), "Data Not Found", Toast.LENGTH_SHORT).show();
+            return isLoginSuccess;
+        }else if(AccountInfo.getAsString("email").equalsIgnoreCase(getUserNameText())){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    private String getUserNameText() {
+        return this.userNameEditText.getText().toString().trim();
+    }
+
+    private String getPasswordText() {
+        return this.passwordEditText.getText().toString().trim();
+    }
+
 }
