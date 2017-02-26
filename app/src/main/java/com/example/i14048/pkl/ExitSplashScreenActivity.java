f77165b4e0ad9ad6fcffa8e342111a878ebaedd2
Kresnadc <kresna_dcahyo@yahoo.co.id>
2017-02-26 18:06:32 +0700

    DateFormat

---
+++ app/src/main/java/com/example/i14048/pkl/ExitSplashScreenActivity.java
@@ -0,0 +1,13 @@
+package com.example.i14048.pkl;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ExitSplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_exit_splash_screen);
+    }
+}
