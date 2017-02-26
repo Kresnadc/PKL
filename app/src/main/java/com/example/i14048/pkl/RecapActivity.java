47a30513564fd45609a87bf7901f32ae152497db
Kresnadc <kresna_dcahyo@yahoo.co.id>
2017-02-26 16:03:01 +0700

    recap

---
+++ app/src/main/java/com/example/i14048/pkl/RecapActivity.java
@@ -0,0 +1,13 @@
+package com.example.i14048.pkl;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RecapActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recap);
+    }
+}
